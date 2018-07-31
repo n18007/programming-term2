3ea8df669b2a84d960499cf3e23d1e8fa1bd3c72
kinjo-icolle <40847104+kinjo-icolle@users.noreply.github.com>
2018-07-31 21:58:06 +0900

    Add files via upload

---
+++ src/algo-p1/task20180801_q03.py
@@ -0,0 +1,8 @@
+# 変数の値を更新してみよう
+money = 2000
+print(money)
+
+# TODO:変数moneyの今の値に5000を足して、変数moneyを上書きしてください
+
+
+# TODO:変数moneyの値を出力してください
---
+++ src/algo-p1/task20180801_q08.py
@@ -0,0 +1,7 @@
+# if〜elseステートメントを使ってみよう
+money = 100
+apple_price = 200
+
+if money >= apple_price:
+    print('りんごを買うことができます')
+# TODO:if文の条件に当てはまらない場合に「お金が足りません」と出力してください
