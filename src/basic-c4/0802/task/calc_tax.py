ac967b1f1cbf7e0e6733fc430aeaa151403338d9
kinjo-icolle <40847104+kinjo-icolle@users.noreply.github.com>
2018-08-02 16:34:14 +0900

    Update calc_tax.py

--- src/basic-c4/0802/task/calc_tax.py
+++ src/basic-c4/0802/task/calc_tax.py
@@ -2,5 +2,18 @@
 TAX_RATE = 8
 
 def calc_incl_tax(excl_tax):
+    """
+    税込み金額を計算する
+    
+    Parameters
+    ----------
+    excl_tax: int
+        税抜き金額
+    
+    Returns
+    -------
+    int
+        税込み金額
+    """
     # TODO:計算した税込金額を返してください。(現状、仮で0を返しています)
     return 0
