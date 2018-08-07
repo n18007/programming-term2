3f5bf1de01fefca0e9cdcac34cb77eea60cf1bf8
kinjo-icolle <40847104+kinjo-icolle@users.noreply.github.com>
2018-08-07 13:59:46 +0900

    Add files via upload

---
+++ src/algo-p2/task20180807_q05.py
@@ -0,0 +1,9 @@
+fruits = {'apple': 100, 'banana': 200, 'orange': 400}
+
+# TODO:キー「banana」の値を数値「300」に更新してください
+
+
+# TODO:キーが「grape」、値が数値の「500」の要素を辞書fruitsに追加してください
+
+
+# TODO:fruitsの値を出力してください
---
+++ src/algo-p2/task20180807_q08.py
@@ -0,0 +1,5 @@
+numbers = [765, 921, 777, 256]
+for number in numbers:
+    print(number)
+    # TODO:変数numberが777のとき「777が見つかったので処理を終了します」と出力した後、処理を終了させてください
+    
