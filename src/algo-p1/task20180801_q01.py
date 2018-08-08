5952aa6dba0ad8f0da886f2865750b088be9a197
n18007 <n18007@std.it-college.ac.jp>
2018-08-08 12:29:20 +0900

    8/8の課題

--- src/algo-p1/task20180801_q01.py
+++ src/algo-p1/task20180801_q01.py
@@ -1,11 +1,14 @@
 # 変数を使ってみよう
-# TODO:変数greetingに文字列「こんにちは」を代入してください
+
+greeting = print("こんにちは")#変数greetingに文字列「こんにちは」を代入してください
 
 
-# TODO:変数greetingの値を出力してください
+greeting = 7
+print(greeting)#変数greetingの値を出力してください
 
 
-# TODO:変数numberに数値の7を代入してください
+number = print("7")#変数numberに数値の7を代入してください
 
 
-# TODO:変数numberの値を出力してください
+number = 7
+print(number)#変数numberの値を出力してください
